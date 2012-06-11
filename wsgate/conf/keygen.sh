53a3d886eec1d6e2f7c0dedbec24da2283144e41
Fritz Elfert <fritz@fritz-elfert.de>
2012-06-11 04:52:17 +0200

    - Adapt keygen script to ubuntu

--- wsgate/conf/keygen.sh
+++ wsgate/conf/keygen.sh
@@ -6,14 +6,22 @@ test -f /etc/sysconfig/wsgate && . /etc/sysconfig/wsgate
 
 test -f $WSGATE_CONFIG || exit 0
 
-CERTFILE=`grep ^certfile $WSGATE_CONFIG|cut -d= -f2|tr -d " "`
-if [ -n "$CERTFILE" -a -f "$CERTFILE" ] ; then
+CERTFILE=`grep ^certfile $WSGATE_CONFIG|head -1|cut -d= -f2|tr -d " "`
+MAKECERT=/etc/pki/tls/certs/make-dummy-cert
+if test -x /usr/sbin/make-ssl-cert -a -d /etc/ssl/certs ; then
+    MAKECERT="/usr/sbin/make-ssl-cert /usr/share/ssl-cert/ssleay.cnf"
+fi
+if test -n "$CERTFILE" -a -f "$CERTFILE" ; then
     exit 0
 fi
-if [ -z "$CERTFILE" ] ; then
-    export CERTFILE=/etc/pki/tls/certs/wsgate.pem
+if test -z "$CERTFILE" ; then
+    if test -x /usr/sbin/make-ssl-cert -a -d /etc/ssl/certs ; then
+        export CERTFILE=/etc/ssl/certs/wsgate.pem
+    else
+        export CERTFILE=/etc/pki/tls/certs/wsgate.pem
+    fi
     perl -pi -e 'if(/^\[([a-z]+)\]/){if($1 eq "ssl"){$ss=1;}else{print "certfile = ".$ENV{"CERTFILE"}."\n" if($ss);}}' $WSGATE_CONFIG
 fi
-/etc/pki/tls/certs/make-dummy-cert "$CERTFILE"
+$MAKECERT "$CERTFILE"
 chmod 0640 "$CERTFILE"
 chgrp wsgate "$CERTFILE"
