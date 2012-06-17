e916c5edb99c62f27596c116147c49a4fd1f6711
Fritz Elfert <fritz@fritz-elfert.de>
2012-06-18 01:22:53 +0200

    - Eliminated compiler warnings in mingw32-build.

--- wsgate/logging.cpp
+++ wsgate/logging.cpp
@@ -17,11 +17,11 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#include "logging.hpp"
-
 #ifdef HAVE_CONFIG_H
 # include "config.h"
 #endif
+
+#include "logging.hpp"
 
 #include <cstdio>
 #include <cstring>
--- wsgate/wsgate_main.cpp
+++ wsgate/wsgate_main.cpp
@@ -21,6 +21,10 @@
 # include "config.h"
 #endif
 
+#ifdef HAVE_WINSOCK2_H
+# include <winsock2.h>
+#endif
+
 #include <ehs.h>
 #include <vector>
 #include <sstream>
