e602cd0149d6951eb9b718a768f027e3e6cacfad
mrd-cloudbase <mario_reja@yahoo.co.uk>
2013-10-15 16:07:19 +0300

    added prereq install path to $HOME/local and updated configure.ac to use that directory as search path/libpath

--- setup-all.sh
+++ setup-all.sh
@@ -3,12 +3,13 @@ set -e
 echo ---- Fetching webconnect dependencies into $(dirname `pwd`)/prereqs ----
 cd ..
 mkdir -p prereqs
+mkdir -p $HOME/local
 cd prereqs
 echo '---- Checking out ehs trunk code ----'
 svn checkout svn://svn.code.sf.net/p/ehs/code/trunk ehs-code
 cd ehs-code
 make -f Makefile.am
-./configure --with-ssl --prefix=$(pwd)/
+./configure --with-ssl --prefix=$HOME/local
 echo '---- Starting ehs build ----'
 make
 echo '---- Finished building ehs ----'
@@ -18,12 +19,13 @@ cd ..
 echo '---- Checking out freerdp master ----'
 git clone https://github.com/FreeRDP/FreeRDP.git
 cd FreeRDP
-mkdir -p build && cd build && cmake -DCMAKE_INSTALL_PREFIX=.. ..
+mkdir -p build && cd build && cmake -DCMAKE_INSTALL_PREFIX=$HOME/local ..
 echo '---- Building freerdp ----'
 make
 echo '---- Finished building freerdp ----'
 make install
 echo '---- Finished installing freerdp ----'
+read -p "Press [Enter] key to start backup..."
 echo '---- Going back to webconnect ----'
 cd ../../../FreeRDP-WebConnect/wsgate/
 make -f Makefile.am
@@ -31,4 +33,4 @@ make -f Makefile.am
 echo '---- Building webconnect ----'
 make
 echo '---- Finished successfully ----'
-echo '---- To run please use `./wsgate -c wsgate.mrd.ini` and connect on localhost:8888 ----'
+echo '---- To run please use `cd wsgate && ./wsgate -c wsgate.mrd.ini` and connect on localhost:8888 ----'
