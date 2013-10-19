5a6cfe068377ba247f1a9555e4b4c69499f136ea
mrd-cloudbase <mario_reja@yahoo.co.uk>
2013-10-19 23:12:53 +0300

    Moved distro-related dependencies install into another script. Updated setup-all.sh to call that script based on commandline issued parameter

---
+++ install_prereqs.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# A script used to install FreeRDP-WebConnect's prerequisites based on the current Distro
+# expects to be invoked with root privileges, either by way of sudo or as the root user itself
+
+if [[ $EUID -ne 0 ]]; then
+   echo "In order to install the dependencies of this package, the script must be run as root (or with sudo)"
+   exit 2
+fi
+
+#Get distro (snipper take from alsa-info.sh)
+DISTRO=`grep -ihs "buntu\|SUSE\|Fedora\|Debian\|Kubuntu\|CentOS" /etc/{issue,*release,*version}`
+case $DISTRO in
+	*buntu*)
+		echo 'Ubuntu distro detected. Installing required packages...'
+		yum install -y git svn subversion-svn2cl openssl-devel boost-devel libpng-devel
+		yum -y groupinstall "Development tools"
+		;;
+	Fedora*19)
+		echo 'Fedora 19 detected.Installing required packages...'
+		;;
+	SUSE*)
+		echo 'SUSE detected. Installing required packages...'
+		;;
+	CentOS*)
+		echo 'CentOS detected. Installing required packages...'
+		yum install -y git svn subversion-svn2cl openssl-devel boost-devel libpng-devel
+		yum groupinstall -y "Development tools"
+		echo 'Getting cmake 2.8.12 (CentOS has old cmake version)'
+		wget http://www.cmake.org/files/v2.8/cmake-2.8.12.tar.gz && tar -xzvf cmake-2.8.12.tar.gz && cd cmake-2.8.12 && ./bootstrap --prefix=/usr && make && make install
+		cd ..
+		rm -rf cmake-2.8.12 && rm -f cmake-2.8.12.tar.gz
+		;;
+		*)
+		echo 'Distro not found! You shall need git, svn-devel, autotools, gcc, g++, boost, openssl-devel and libpng-devel to successfully build this package.'
+		echo 'For tracing to work, libdwarf is also required.'
+		exit 1
+		;;
+esac
--- setup-all.sh
+++ setup-all.sh
@@ -1,73 +1,106 @@
-#!/bin/bash -
-set -e
-#Variable which determines if we install prereqs or not
-cont=1
-if [[ $EUID -ne 0 ]]; then
-   echo "In order to install the dependencies of this package, the script must be run as root (or with sudo)"
-   echo -n "However, if all dependencies are installed (see README for a list), do you wish to proceed? [y/N] "
-   read response
-   [[ $response == "y" ]] && cont=2 || exit 1
+#!/bin/bash
+
+# Script used to install FreeRDP-WebConnect on the local machine
+
+# Exit status:
+# 0 = Success
+# 1 = improper command-line arguments
+# 2 = executed by root, but no force-root option provided
+# 3 = install dependencies failed
+# 4 = failed to build ehs package
+# 5 = failed to install ehs package
+# 6 = failed to build FreeRDP package
+# 7 = failed to install FreeRDP package
+# 8 = failed to build FreeRDP-WebConnect package
+# 99 = failed to execute some shell command
+
+USAGE=$(basename $0) [-f|--force-root] [-i|--install-deps]
+# Root is not supposed to run this script. If however -f or --force-root is given on the command-line
+# this rule will not be enforced. Otherwise, the script will exit.
+
+if [[ $# -gt 1 ]]; then
+	# Get command-line options
+	TEMP=`getopt -o fi --long force-root,install-deps -q -- "$@"`
+	# getopt failed because of improper arguments. Terminate script.
+	if [ $? != 0 ] ; then echo "$USAGE" >&2 ; exit 1 ; fi
+	# preserve whitespace
+	eval set -- "$TEMP"
+	# Parse command-line arguments
+	while true ; do
+		case "$1" in
+			-f|--force-root) force_root=1 ; shift ;;
+			-i|--install-deps) instal_deps=1 ; shift ;;
+			--) shift ; break ;;
+			*) echo "Internal error while parsing command-line. Exiting..." ; exit 1 ;;
+		esac
+	done
 fi
-if [[ $cont -eq 1 ]]; then
-	#Get distro (snipper take from alsa-info.sh)
-	DISTRO=`grep -ihs "buntu\|SUSE\|Fedora\|Debian\|Kubuntu\|CentOS" /etc/{issue,*release,*version}`
-	case $DISTRO in
-		*buntu*)
-			echo 'Ubuntu distro detected. Installing required packages...'
-			yum install -y git svn subversion-svn2cl openssl-devel boost-devel libpng-devel
-			yum -y groupinstall "Development tools"
-			;;
-		Fedora*19)
-			echo 'Fedora 19 detected.Installing required packages...'
-			;;
-		SUSE*)
-			echo 'SUSE detected. Installing required packages...'
-			;;
-		CentOS*)
-			echo 'CentOS detected. Installing required packages...'
-			yum install -y git svn subversion-svn2cl openssl-devel boost-devel libpng-devel
-			yum groupinstall -y "Development tools"
-			echo 'Getting cmake 2.8.12 (CentOS has old cmake version)'
-			wget http://www.cmake.org/files/v2.8/cmake-2.8.12.tar.gz && tar -xzvf cmake-2.8.12.tar.gz && cd cmake-2.8.12 && ./bootstrap --prefix=/usr && make && make install
-			cd ..
-			rm -rf cmake-2.8.12 && rm -f cmake-2.8.12.tar.gz
-			;;
-			*)
-			echo 'Distro not found! You shall need git, svn-devel, autotools, gcc, g++, boost, openssl-devel and libpng-devel to successfully build this package.'
-			echo 'For tracing to work, libdwarf is also required.'
-			;;
-	esac
+
+if [[ $UID -eq 0 && force_root -ne 1 ]]; then
+   echo "If you wish to run this script as root, use the --force-root option."
+   exit 2
 fi
+
+# Install_deps set
+if [[ $install_deps -eq 1 ]]; then
+	echo 'Preparing to install package dependencies'
+	
+	# If script executed by root and force_root enabled, then just install dependencies
+	if [[ $UID -eq 0 && force_root -eq 1 ]]; then
+		./install_prereqs.sh
+		if [[ $? -ne 0 ]]; then
+			echo 'Unable to install dependencies. '
+			exit 3
+		fi
+	else
+	# Try sudo command. If sudo not present, try to su - to root.
+		command -v sudo >/dev/null 2>&1 && sudo_present=1
+		if [[ sudo_present -eq 1 ]]; then
+			echo 'sudo available. Please enter your password: '
+			sudo ./install_prereqs.sh
+			if [[ $? -ne 0 ]]; then
+				exit 3
+			fi
+		else
+			echo 'sudo command unavailable. Please enter root password: '
+			su -c ./install_prereqs.sh
+			if [[ $? -ne 0 ]]; then
+				exit 3
+			fi
+		fi
+	fi
+fi
+
 echo ---- Fetching webconnect dependencies into $(dirname `pwd`)/prereqs ----
-cd ..
-mkdir -p prereqs
-mkdir -p $HOME/local
-cd prereqs
+cd .. || exit 99
+mkdir -p prereqs || exit 99
+mkdir -p $HOME/local || exit 99
+cd prereqs || exit 99
 echo '---- Checking out ehs trunk code ----'
-svn checkout svn://svn.code.sf.net/p/ehs/code/trunk ehs-code
-cd ehs-code
-make -f Makefile.am
-./configure --with-ssl --prefix=$HOME/local
+svn checkout svn://svn.code.sf.net/p/ehs/code/trunk ehs-code || echo 'Failed to download ehs code from svn'; exit 99
+cd ehs-code || exit 99
+make -f Makefile.am || exit 4
+./configure --with-ssl --prefix=$HOME/local || exit 4
 echo '---- Starting ehs build ----'
-make
+make || exit 4
 echo '---- Finished building ehs ----'
-make install
+make install || exit 5
 echo '---- Finished installing ehs ----'
-cd ..
+cd .. || exit 99
 echo '---- Checking out freerdp master ----'
-git clone https://github.com/FreeRDP/FreeRDP.git
-cd FreeRDP
-mkdir -p build && cd build && cmake -DCMAKE_INSTALL_PREFIX=$HOME/local .. -DLIB_SUFFIX=64
+git clone https://github.com/FreeRDP/FreeRDP.git || echo 'Failed to download FreeRDP code from github.'; exit 99
+cd FreeRDP || exit 99
+mkdir -p build && cd build && cmake -DCMAKE_INSTALL_PREFIX=$HOME/local .. -DLIB_SUFFIX=64 || exit 6
 echo '---- Building freerdp ----'
-make
+make || exit 6
 echo '---- Finished building freerdp ----'
-make install
+make install || exit 7
 echo '---- Finished installing freerdp ----'
 echo '---- Going back to webconnect ----'
-cd ../../../FreeRDP-WebConnect/wsgate/
-make -f Makefile.am
-./configure
+cd ../../../FreeRDP-WebConnect/wsgate/ || exit 99
+make -f Makefile.am || exit 8
+./configure || exit 8
 echo '---- Building webconnect ----'
-make
+make || exit 8
 echo '---- Finished successfully ----'
 echo '---- To run please use `cd wsgate && ./wsgate -c wsgate.mrd.ini` and connect on localhost:8888 ----'
