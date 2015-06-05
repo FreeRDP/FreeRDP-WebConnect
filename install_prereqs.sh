250a5c3e69bd1e04d932e919c2ac66b118984878
Cosmin Munteanu <cmunteanu@cloudbasesolutions.com>
2015-06-05 22:28:19 +0300

    Removed Ubuntu 12,13 from install_prereqs.sh - not supported anymore

--- install_prereqs.sh
+++ install_prereqs.sh
@@ -47,33 +47,6 @@ case $DISTRO in
 		  remmina remmina-common remmina-plugin-rdp remmina-plugin-vnc remmina-plugin-gnome \
 		  remmina-plugin-nx remmina-plugin-telepathy remmina-plugin-xdmcp
 		;;
-	*buntu*12*)
-		echo 'Ubuntu 12.04 detected. Installing required packages...'
-		apt-get update
-		apt-get install -y python-software-properties
-		echo | add-apt-repository ppa:ubuntu-toolchain-r/test  
-		apt-get update  
-		apt-get install -y build-essential g++-4.8 libxml++2.6-dev libssl-dev \
-		libboost1.48-all-dev libpng-dev libdwarf-dev subversion subversion-tools \
-		autotools-dev autoconf libtool cmake
-		# replace old gcc/g++ with new one
-		rm /usr/bin/g++  
-		ln -s /usr/bin/g++-4.8 /usr/bin/g++  
-		rm /usr/bin/gcc  
-		ln -s /usr/bin/gcc-4.8 /usr/bin/gcc 
-		;;
-	*buntu*13*)
-		echo 'Ubuntu 13.10 detected. Installing required packages...'
-		apt-get update  
-		apt-get install -y build-essential g++-4.8 libxml++2.6-dev libssl-dev \
-		libboost1.49-all-dev libpng-dev libdwarf-dev subversion subversion-tools svn2cl \
-		autotools-dev autoconf libtool cmake
-		# replace old gcc/g++ with new one
-		rm /usr/bin/g++  
-		ln -s /usr/bin/g++-4.8 /usr/bin/g++  
-		rm /usr/bin/gcc  
-		ln -s /usr/bin/gcc-4.8 /usr/bin/gcc 
-		;;
 	Fedora*)
 		echo 'Fedora detected.Installing required packages...'
 		yum install -y gcc-c++ autoconf automake libtool cmake svn svn2cl \
