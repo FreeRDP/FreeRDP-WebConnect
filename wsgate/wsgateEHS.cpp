8a0644664183f0f4e00e6b09a15bcd2e3b63bc98
Cosmin Munteanu <cmunteanu@cloudbasesolutions.com>
2015-09-29 20:05:46 -0700

    Removed Nova code

--- wsgate/wsgateEHS.cpp
+++ wsgate/wsgateEHS.cpp
@@ -1,5 +1,6 @@
 #include "wsgateEHS.hpp"
 #include "wsgate.hpp"
+#include "pluginManager.h"
 
 namespace wsgate{
     WsGate::MimeType WsGate::simpleMime(const string & filename)
@@ -273,40 +274,17 @@ namespace wsgate{
         bool setCookie = true;
         EmbeddedContext embeddedContext = CONTEXT_PLAIN;
 
-        if(boost::starts_with(uri, "/wsgate?token="))
-        {
-            // OpenStack console authentication
+        std::map<std::string, std::string> pluginOutput;
+        if (PluginManager::getInstance()->queryPlugins(uri, pluginOutput)){
+            //wsgate will get auth info from the plugin
             setCookie = false;
-            try
-            {
-                log::info << "Starting OpenStack token authentication" << endl;
+            embeddedContext = CONTEXT_EMBEDDED;
 
-                string tokenId = request->FormValues("token").m_sBody;
-
-                nova_console_token_auth* token_auth = nova_console_token_auth_factory::get_instance();
-
-                nova_console_info info = token_auth->get_console_info(m_sOpenStackAuthUrl, m_sOpenStackUsername,
-                                                                        m_sOpenStackPassword, m_sOpenStackTenantName,
-                                                                        tokenId);
-
-                log::info << "Host: " << info.host << " Port: " << info.port
-                            << " Internal access path: " << info.internal_access_path
-                            << endl;
-
-                rdphost = info.host;
-                rdpport = info.port;
-                rdppcb = info.internal_access_path;
-
-                rdpuser = m_sHyperVHostUsername;
-                rdppass = m_sHyperVHostPassword;
-
-                embeddedContext = CONTEXT_EMBEDDED;
-            }
-            catch(exception& ex)
-            {
-                log::err << "OpenStack token authentication failed: " << ex.what() << endl;
-                return HTTPRESPONSECODE_400_BADREQUEST;
-            }
+            rdphost = pluginOutput["rdphost"];
+            rdpport = std::stoi(pluginOutput["rdpport"]);
+            rdppcb = pluginOutput["rdppcb"];
+            rdpuser = pluginOutput["rdpuser"];
+            rdppass = pluginOutput["rdppass"];
         }
 
         params =
