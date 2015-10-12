df226ede17201d50abbb132a92ce485788b5e818
Cosmin Munteanu <cmunteanu@cloudbasesolutions.com>
2015-10-12 20:27:54 +0300

    Added cmake definition for pluginOpenstack

---
+++ wsgate/pluginOpenstack/config.h.cmake
@@ -0,0 +1,14 @@
+/* Defined if the requested minimum BOOST version is satisfied */
+#cmakedefine HAVE_BOOST 1
+
+/* Define to 1 if you have the <windows.h> header file. */
+#cmakedefine HAVE_WINDOWS_H 1
+
+/* Define to empty if `const' does not conform to ANSI C. */
+#cmakedefine const 1
+
+/* Define to `__inline__' or `__inline' if that's what the C compiler
+   calls it, or to nothing if 'inline' is not supported under any name.  */
+#ifndef __cplusplus
+#cmakedefine inline 1
+#endif
