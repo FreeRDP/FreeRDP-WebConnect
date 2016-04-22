baa5548e3da9a77e404ce6c1ce941dd641402f6c
Cosmin Munteanu <cmunteanu@cloudbasesolutions.com>
2016-04-22 17:49:56 +0300

    Effectuate override before connection

--- wsgate/myrawsocket.cpp
+++ wsgate/myrawsocket.cpp
@@ -61,9 +61,32 @@ namespace wsgate{
         return true;
     }
 
-    void MyRawSocketHandler::PrepareRDP(const std::string host, const std::string pcb, const std::string user, const std::string pass, const WsRdpParams &params){
+    void MyRawSocketHandler::PrepareRDP(const std::string _host, const std::string _pcb, const std::string _user, const std::string _pass, const WsRdpParams &_params){
+        std::string host = _host;
+        std::string pcb = _pcb;
+        std::string user = _user;
+        std::string pass = _pass;
+        WsRdpParams params = _params;
+
         string username;
         string domain;
+
+        //do needed overrides
+        WsRdpOverrideParams op = this->m_parent->getOverrideParams();
+        if (op.m_bOverrideRdpFntlm) params.fntlm = op.m_RdpOverrideParams.fntlm;
+        if (op.m_bOverrideRdpNomani) params.nomani = op.m_RdpOverrideParams.nomani;
+        if (op.m_bOverrideRdpNonla) params.nonla = op.m_RdpOverrideParams.nonla;
+        if (op.m_bOverrideRdpNotheme) params.notheme = op.m_RdpOverrideParams.notheme;
+        if (op.m_bOverrideRdpNotls) params.notls = op.m_RdpOverrideParams.notls;
+        if (op.m_bOverrideRdpNowallp) params.nowallp = op.m_RdpOverrideParams.nowallp;
+        if (op.m_bOverrideRdpNowdrag) params.nowdrag = op.m_bOverrideRdpNowdrag;
+        if (op.m_bOverrideRdpPerf) params.perf = op.m_RdpOverrideParams.perf;
+        if (op.m_bOverrideRdpPort) params.port = op.m_RdpOverrideParams.port;
+        if (op.m_bOverrideRdpHost) host = op.m_sRdpOverrideHost;
+        if (op.m_bOverrideRdpPass) pass = op.m_sRdpOverridePass;
+        if (op.m_bOverrideRdpPcb) pcb = op.m_sRdpOverridePcb;
+        if (op.m_bOverrideRdpUser) user = op.m_sRdpOverrideUser;
+
         SplitUserDomain(user, username, domain);
 
         rdp_ptr r = this->m_cmap[this->conn].get<2>();
@@ -79,7 +102,7 @@ namespace wsgate{
         log::debug << "RDP No wallpaper:       " << params.nowallp << endl;
         log::debug << "RDP No full windowdrag: " << params.nowdrag << endl;
         log::debug << "RDP No menu animation:  " << params.nomani << endl;
-        log::debug << "RDP No theming:         " << params.nomani << endl;
+        log::debug << "RDP No theming:         " << params.notheme << endl;
         log::debug << "RDP Disable TLS:        " << params.notls << endl;
         log::debug << "RDP Disable NLA:        " << params.nonla << endl;
         log::debug << "RDP NTLM auth:          " << params.fntlm << endl;
